3dd5794847bf7febed373039ab5dec530c2f2cc5
Phanel Xavier <phanel.xavier@gmail.com>
2020-05-18 17:38:05 -0400

    Added field age

--- src/org/softwisetech/model/User.java
+++ src/org/softwisetech/model/User.java
@@ -2,4 +2,22 @@ package org.softwisetech.model;
 
 public class User {
 
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + "]";
+	}
+	
+	
+
 }
