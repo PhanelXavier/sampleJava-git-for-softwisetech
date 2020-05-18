efd0327b83233f7603bf36b9f39d38709315ede1
Phanel Xavier <phanel.xavier@gmail.com>
2020-05-18 17:55:35 -0400

    Added getter and setter for age

--- src/org/softwisetech/model/User.java
+++ src/org/softwisetech/model/User.java
@@ -3,6 +3,17 @@ package org.softwisetech.model;
 public class User {
 
 	private String name;
+	private int age;
+	
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
@@ -12,4 +23,16 @@ public class User {
 		this.name = name;
 	}
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
 }
